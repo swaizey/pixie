e2f670426d693c8a2a8558fe09bef6d6adcc1b80
swaizey <64682406+swaizey@users.noreply.github.com>
2024-11-16 15:48:02 +0100

    Update route.js

--- app/api/chat/[...single]/route.js
+++ app/api/chat/[...single]/route.js
@@ -6,7 +6,7 @@ export async function GET(request, {params}){
     const members = params.single
     const members2 = params.single.toReversed()
     connectMongoDB()
-    console.log(params)
+  
     try {
         const chatExist = await chatSchema.find({$or:[{members:members2},{members:members}]})
     return NextResponse.json(chatExist)
