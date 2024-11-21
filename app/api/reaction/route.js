07d58e8b803cc7a4aa9970875e9ae57645326056
swaizey <64682406+swaizey@users.noreply.github.com>
2024-11-21 12:30:05 +0100

    Update route.js

--- app/api/reaction/route.js
+++ app/api/reaction/route.js
@@ -15,7 +15,7 @@ export async function GET(request) {
     try{
       const react = await Post.findById({_id:id}).sort({createdAt:-1})
   
-      return NextResponse.json(react?.reaction);
+      return NextResponse.json(react);
     }catch(error){
       return NextResponse.json({msg:"Something went wrong"},{id:id},{status:500})
     }
