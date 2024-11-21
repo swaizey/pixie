5c6bdc5b8f7814542c73c4a11baddfe05f5cbd3c
swaizey <64682406+swaizey@users.noreply.github.com>
2024-11-21 12:53:44 +0100

    Update Reactions.js

--- app/component/reactions/Reactions.js
+++ app/component/reactions/Reactions.js
@@ -52,7 +52,7 @@ const Reactions = ({opened, setOpened, postId, setPostId}) => {
   return (
     <div className={`${Style.msgCon} ${opened ? null : Style.closed}`}>
     
-        <button onClick={()=>(setOpened(false), setPostId(null))} className={Style.btn}>x</button>
+        <button onClick={()=>(setOpened(false), postId=null)} className={Style.btn}>x</button>
         <div style={{display:'grid', gridTemplateRows:'auto 40px', height:'90%'}}>
         <div style={{overflowY:'auto'}}>
         {msg?.reaction?.length == 0 ? (<p>No post now</p>) : msg?.reaction?.map(m=>(
