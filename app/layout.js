6a4ab858b7ab3b17a7b9b0a2c50784672f0bc8dc
swaizey <64682406+swaizey@users.noreply.github.com>
2024-11-26 15:12:35 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -31,7 +31,7 @@ export default function RootLayout({ children }) {
         <AuthProvider>
         <UserProvider>
       
-        {children}
+        <div style={{heigth:'90vh'}}>{children}</div>
           <Footer />
   
         </UserProvider>
