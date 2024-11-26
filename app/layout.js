1c05434d0d17b4e5600a19d1564ec82d627f455c
swaizey <64682406+swaizey@users.noreply.github.com>
2024-11-26 14:35:30 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -23,6 +23,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
       <script src="https://upload-widget.cloudinary.com/latest/global/all.js" type="text/javascript">  
       </script>
       </head>
